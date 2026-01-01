9da8d4d91a241e76a3af67387e5e14c84ce160f0
adem mbarek <ademmbarek4@gmail.com>
2026-01-02 00:42:55 +0100

    dernier modification

--- brute_force.py
+++ brute_force.py
@@ -3,34 +3,40 @@ from wordfreq import zipf_frequency
 
 
 def score_with_wordfreq(text):
-    words = text.lower().split()
-    score_fr = 0
-    score_en = 0
+    # Calcule un score pour le français et l’anglais
 
-    for w in words:
-        score_fr += zipf_frequency(w, "fr")
-        score_en += zipf_frequency(w, "en")
+    words = text.lower().split()  # Sépare le texte en mots dans une list
+    score_fr = 0                  
+    score_en = 0                  
 
-    return score_fr, score_en
+    for w in words:               
+        score_fr += zipf_frequency(w, "fr")  # Fréquence du mot en français
+        score_en += zipf_frequency(w, "en")  # Fréquence du mot en anglais
+
+    return score_fr, score_en     
 
 
 def break_caesar(ciphertext):
-    best_score = -1
-    best_result = {
+
+    best_score = -1               # Meilleur score trouvé
+    best_result = {               # un enregistrement du Résultat final
         "key": None,
         "text": "",
         "score": 0,
         "language": ""
     }
 
-    for key in range(26):
-        decrypted = caesar_decrypt(ciphertext, key)
+    for key in range(26):         # Teste toutes les clés possibles
+        decrypted = caesar_decrypt(ciphertext, key)  # Déchiffre le texte
 
+        # Calcule les scores linguistiques
         score_fr, score_en = score_with_wordfreq(decrypted)
         total_score = max(score_fr, score_en)
 
+        # Affiche les résultats intermédiaires
         print(key, decrypted, "FR:", score_fr, "EN:", score_en)
 
+        # Garde le meilleur résultat
         if total_score > best_score:
             best_score = total_score
             best_result["key"] = key
@@ -38,4 +44,4 @@ def break_caesar(ciphertext):
             best_result["score"] = total_score
             best_result["language"] = "Français" if score_fr > score_en else "Anglais"
 
-    return best_result
+    return best_result 
--- caesar.py
+++ caesar.py
@@ -1,11 +1,18 @@
 def caesar_decrypt(text, key):
-    result = ""
+    # Cette fonction déchiffre un texte avec le chiffrement de César
+    
 
-    for char in text:
-        if char.isalpha():
+    result = ""  # Variable pour stocker le texte déchiffré
+
+    for char in text:  # Parcourt chaque caractère du texte
+
+        if char.isalpha():  
+            # Choisit la base selon majuscule ou minuscule
             base = ord('a') if char.islower() else ord('A')
+            # Applique le décalage inverse pour déchiffrer
             result += chr((ord(char) - base - key) % 26 + base)
         else:
+            # Garde les espaces et symboles inchangés
             result += char
 
-    return result
+    return result  
